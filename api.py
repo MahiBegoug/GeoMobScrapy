828440d2496643df7abee0009eb41da5bc817d72
Mahi Begoug <gm_begoug@esi.dz>
2020-06-30 16:06:57 +0100

    add wikipedia

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request, jsonify
 from youtube_api import  YouTubeDataAPI
+import wikipedia
+
 YT_KEY = 'AIzaSyAlMc2u9oSzR-KQw3FAo7MC3fMI3yMWvSo' # you can hardcode this, too.
 yt = YouTubeDataAPI(YT_KEY)
 
@@ -12,6 +14,25 @@ app.config["DEBUG"] = True
 @app.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
+
+@app.route("/api/wiki",methods=['GET'])
+def wiki_name():
+    # check if an name Country provided
+    # if ID is provided, assing it to a variable
+    # if no ID is provided, display an error in the browser
+    if 'name' in request.args:
+        country_name = str(request.args['name'])
+        wikipedia.set_lang("en")
+        wikipage = wikipedia.page(country_name)
+        data = {
+            'title': wikipage.title,
+            'url':wikipage.url,
+            'summary':wikipage.summary,
+            'content':wikipage.content,
+            'images':wikipage.images,
+            'mainImage':wikipage.images[0]
+        }
+        return jsonify(data)
 
 @app.route('/api/country', methods=['GET'])
 def country_name():
